348427a19b0433f613ec85dc727b25c974eb6442
BenyBoy84 <Blais_Benoit@hotmail.com>
2023-11-02 12:51:22 -0400

    v1.0.1

--- data_organization.tf
+++
@@ -1,4 +0,0 @@
-# The following block is use to get information about an organization.
-data "tfe_organization" "this" {
-  name = local.organization_name
-}
--- locals.tf
+++ locals.tf
@@ -1,6 +1,4 @@
 locals {
-  # This local is used to define the organization name.
-  organization_name = "ConseilsTI"
 
   # This local is used to define Oauth_client name.
   oauth_client_name = "GitHub.com"
