7be40340bd76e89d80072aebc268b5ace5f0ee31
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-01-22 18:16:35 -0500

    ft: GitHub

--- data_oauth_client.tf
+++
@@ -1,5 +0,0 @@
-# The following block is use to get information about an OAuth client.
-data "tfe_oauth_client" "client" {
-  organization = var.organization_name
-  name         = var.oauth_client_name
-}
--- versions.tf
+++ versions.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/tfe"
       version = "0.48.0"
     }
+    github = {
+      source  = "integrations/github"
+      version = "5.44.0"
+    }
   }
 
   required_version = "> 1.6.0"
