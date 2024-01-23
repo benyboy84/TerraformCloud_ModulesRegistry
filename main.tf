530bdbe62de89221ba2fc11e7cf7104af0c03331
BenyBoy84 <Blais_Benoit@hotmail.com>
2024-01-23 07:53:33 -0500

    ft: GitHub

--- main.tf
+++ main.tf
@@ -6,14 +6,26 @@ data "tfe_oauth_client" "client" {
 
 # The following code block is used to create GitHub repository.
 
-module "repository" {
-  source = "./modules/github_repository"
+resource "github_repository" "this" {
 
   for_each = toset(var.modules_name)
 
   name                   = each.value
-  description            = "Terraform module."
+  description            = "Terraform module to manage ${element(split("-", each.value), 1) resources.}"
+  visibility             = "public"
+  has_issues             = true
+  has_discussions        = false
+  has_projects           = true
+  has_wiki               = true
+  is_template            = false
+  allow_merge_commit     = true
+  allow_squash_merge     = true
+  allow_rebase_merge     = true
+  allow_auto_merge       = false
   delete_branch_on_merge = true
+  auto_init              = false
+  archived               = false
+  archive_on_destroy     = false
   security_and_analysis = {
     # advanced_security = {
     #   status = try(each.value.github_repository.security_and_analysis.advanced_security.status, null)
@@ -25,37 +37,67 @@ module "repository" {
       status = "enabled"
     }
   }
-  vulnerability_alerts = true
-  allow_update_branch  = false
-  branch_protections = [
-    {
-      pattern                         = "main"
-      enforce_admins                  = true
-      require_conversation_resolution = true
-      required_pull_request_reviews = {
-        dismiss_stale_reviews           = true
-        require_code_owner_reviews      = true
-        required_approving_review_count = "0"
-      }
-    }
-  ]
-  allowed_actions = "selected"
-  allowed_actions_config = {
-    github_owned_allowed = true
-    patterns_allowed     = ["terraform-docs/gh-actions@*", "super-linter/super-linter@*", "rymndhng/release-on-push-action@*", "hashicorp/*"]
-  }
-  files = [
-    {
-      file    = ".gitignore" 
-      content = file("./data/.gitignore")
-    }
-  ]
-  branches = [
-    {
-      branch        = "test"
-    }
-  ]
+  vulnerability_alerts                    = true
+  ignore_vulnerability_alerts_during_read = false
+  allow_update_branch                     = false
+
 }
+
+
+
+
+
+
+# module "repository" {
+#   source = "./modules/github_repository"
+
+#   for_each = toset(var.modules_name)
+
+#   name                   = each.value
+#   description            = "Terraform module."
+#   delete_branch_on_merge = true
+#   security_and_analysis = {
+#     # advanced_security = {
+#     #   status = try(each.value.github_repository.security_and_analysis.advanced_security.status, null)
+#     # }
+#     secret_scanning = {
+#       status = "enabled"
+#     }
+#     secret_scanning_push_protection = {
+#       status = "enabled"
+#     }
+#   }
+#   vulnerability_alerts = true
+#   allow_update_branch  = false
+#   branch_protections = [
+#     {
+#       pattern                         = "main"
+#       enforce_admins                  = true
+#       require_conversation_resolution = true
+#       required_pull_request_reviews = {
+#         dismiss_stale_reviews           = true
+#         require_code_owner_reviews      = true
+#         required_approving_review_count = "0"
+#       }
+#     }
+#   ]
+#   allowed_actions = "selected"
+#   allowed_actions_config = {
+#     github_owned_allowed = true
+#     patterns_allowed     = ["terraform-docs/gh-actions@*", "super-linter/super-linter@*", "rymndhng/release-on-push-action@*", "hashicorp/*"]
+#   }
+#   files = [
+#     {
+#       file    = ".gitignore" 
+#       content = file("./data/.gitignore")
+#     }
+#   ]
+#   branches = [
+#     {
+#       branch        = "test"
+#     }
+#   ]
+# }
 
 # The following code block is used to create module resources in the private registry.
 
